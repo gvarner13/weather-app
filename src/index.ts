87b54a25d99d6c942623be041bec9c718336a20c
Gary Varner <garysarahvarner@gmail.com>
2023-12-11 21:30:04 -0500

    changed ENV var and added type for it

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,11 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	WEATHER_TOKEN: string;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		let endpoint = 'https://api.waqi.info/feed/geo:';
-		const token = env.weather_token; //Use a token from https://aqicn.org/api/
+		const token = env.WEATHER_TOKEN; //Use a token from https://aqicn.org/api/
 
 		const latitude = request.cf?.latitude;
 		const longitude = request.cf?.longitude;
@@ -40,7 +17,7 @@ export default {
 		};
 
 		const response = await fetch(endpoint, init);
-		const content = await response.json();
+		const content: object = await response.json();
 
 		let html_content = `<p>You are located at: ${latitude},${longitude}.</p>`;
 		html_content += `<p>Based off sensor data from <a class="underline decoration-lime-400" href="${content.data.city.url}">${content.data.city.name}</a>:</p>`;
