84a300a339098dc54e960d8e5976c0acb2a014cb
Gary Varner <garysarahvarner@gmail.com>
2024-01-01 19:54:55 -0500

    removed periods as they look odd and changed the bg color

--- src/index.ts
+++ src/index.ts
@@ -21,10 +21,10 @@ export default {
 
 		let html_content = `<p>You are located at: ${latitude},${longitude}.</p>`;
 		html_content += `<p>Based off sensor data from <a class="underline decoration-lime-400" href="${content.data.city.url}">${content.data.city.name}</a>:</p>`;
-		html_content += `<p>The AQI level is: ${content.data.aqi}.</p>`;
-		html_content += `<p>The N02 level is: ${content.data.iaqi.no2?.v}.</p>`;
-		html_content += `<p>The O3 level is: ${content.data.iaqi.o3?.v}.</p>`;
-		html_content += `<p>The temperature is: ${content.data.iaqi.t?.v}°C.</p>`;
+		html_content += `<p>The AQI level is: ${content.data.aqi}</p>`;
+		html_content += `<p>The N02 level is: ${content.data.iaqi.no2?.v}</p>`;
+		html_content += `<p>The O3 level is: ${content.data.iaqi.o3?.v}</p>`;
+		html_content += `<p>The temperature is: ${content.data.iaqi.t?.v}°C</p>`;
 
 		let html = `
       <!DOCTYPE html>
@@ -33,7 +33,7 @@ export default {
 		<link rel="icon" href="https://fav.farm/%F0%9F%8C%A6" />
         <script src="https://cdn.tailwindcss.com"></script>
       </head>
-      <body class="bg-slate-900">
+      <body class="bg-slate-700">
         <main class="flex  flex-col items-center w-[400px] m-auto">
           <div id="container" class="mt-6 p-6">
             <h1 class="text-center text-9xl font-black" style="background: linear-gradient(to right, #E7FF52, #41FF54);-webkit-background-clip: text;-webkit-text-fill-color: transparent">Your Localish Weather</h1>
